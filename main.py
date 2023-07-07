430b95f9560b9c285bc8392107c94b8b1bf58f13
Ken Ojiri <kojiri@vmware.com>
2023-07-07 04:13:51 +0000

    fix listening port number

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+  return "Hello World!"
 
-if __name__ == '__main__':
-    app.run()
+#see: https://docs.cloudfoundry.org/buildpacks/python/index.html#run-web-server
+if __name__ == "__main__":
+  port = int(os.getenv("PORT", 8080))
+  app.run(host='0.0.0.0', port=port)
